1f1de8c935580c6c31090a044d7e426e5cf3a726
SelfishHawk <srivastavasrijan138@gmail.com>
2025-03-17 10:27:21 +0530

    Deleting the middle of stack using recursion

--- Stack/4_delete_Middle.cpp
+++ Stack/4_delete_Middle.cpp
@@ -1,14 +1,27 @@
 #include<iostream>
 #include<stack>
 using namespace std;
+void solve(stack<int> &org,int size,int n){
+    if(n==size/2){
+        org.pop();
+        return;
+    }
+    int temp=org.top();
+    org.pop();
+    solve(org,size,n+1);
+    org.push(temp);
+}
 int main(){
     stack<int> org;
     org.push(1);
     org.push(2);
     org.push(3);
     org.push(4);
-    stack<int> temp;
+    org.push(5);
     int size=org.size();
+    int n=0;
+    solve(org,size,n);
+/*    stack<int> temp;
     int mid=size/2;
     for(int i=0;i<mid;i++){
         temp.push(org.top());
@@ -19,7 +32,8 @@ while(!temp.empty()){
     org.push(temp.top());
     temp.pop();
 }
-while(!org.empty()){
+    */
+   while(!org.empty()){
     cout<<" "<<org.top();
     org.pop();
 }
