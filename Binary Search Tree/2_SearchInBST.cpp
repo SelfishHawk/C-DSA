7687db5829897e0e347d7adef34e30f383b28b10
SelfishHawk <srivastavasrijan138@gmail.com>
2025-04-13 10:19:37 +0530

    Iterative way to find a data in BST

--- Binary Search Tree/2_SearchInBST.cpp
+++ Binary Search Tree/2_SearchInBST.cpp
@@ -1,62 +1,91 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
 using namespace std;
-class node{
-    public:
+class node
+{
+public:
     int data;
-    node* left;
-    node* right;
-    node(int d){
-        this->data=d;
-        this->left=NULL;
-        this->right=NULL;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
-node* insertIntoBST(node* &root,int d){
-    if(root==NULL){
+node *insertIntoBST(node *&root, int d)
+{
+    if (root == NULL)
+    {
         return new node(d);
     }
-    if(d>root->data){
-        root->right=insertIntoBST(root->right,d);
-    }else{
-        root->left=insertIntoBST(root->left,d);
+    if (d > root->data)
+    {
+        root->right = insertIntoBST(root->right, d);
+    }
+    else
+    {
+        root->left = insertIntoBST(root->left, d);
     }
     return root;
 }
-node* insert(node* &root){
+node *insert(node *&root)
+{
     int d;
-    cin>>d;
-    while(d!=-1){
-    root=insertIntoBST(root,d);
-    cin>>d;
+    cin >> d;
+    while (d != -1)
+    {
+        root = insertIntoBST(root, d);
+        cin >> d;
     }
     return root;
 }
-bool BSTsearch(node* root,int val){
-    if(root==NULL){
-        return false;
+bool BSTsearch(node *root, int val)
+{
+    // if(root==NULL){
+    //     return false;
+    // }
+    // if(root->data==val){
+    //     return true;
+    // }
+    // if(root->data>val){
+    //     return BSTsearch(root->left,val);
+    // }else{
+    //     return BSTsearch(root->right,val);
+    // }
+    node *temp = root;
+    while (temp)
+    {
+        if (temp->data == val)
+        {
+            return true;
+        }
+    if (temp->data > val)
+    {
+        return BSTsearch(temp->left, val);
     }
-    if(root->data==val){
-        return true;
-    }
-    if(root->data>val){
-        return BSTsearch(root->left,val);
-    }else{
-        return BSTsearch(root->right,val);
-
+    else
+    {
+        return BSTsearch(temp->right, val);
     }
 }
-int main(){
-    node* root=NULL;
-    cout<<"Insert the data"<<endl;
+return false;
+}
+int main()
+{
+    node *root = NULL;
+    cout << "Insert the data" << endl;
     insert(root);
-    cout<<"Enter the data to search "<<endl;
+    cout << "Enter the data to search " << endl;
     int val;
-    cin>>val;
-    if(BSTsearch(root,val)==true){
+    cin >> val;
+    if (BSTsearch(root, val) == true)
+    {
         cout << val << " is found in the BST." << endl;
-    }else{
+    }
+    else
+    {
         cout << val << " is not found in the BST." << endl;
-
     }
 }
